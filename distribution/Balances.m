44f634fbe8a40bfbf728cd94421e47129d703294
Jeffrey Varner <jdv27@cornell.edu>
2016-09-27 12:35:57 -0400

    Balances.m

---
+++ distribution/Balances.m
@@ -0,0 +1,58 @@
+% ----------------------------------------------------------------------------------- %
+% Copyright (c) 2016 Varnerlab
+% Robert Frederick School of Chemical and Biomolecular Engineering
+% Cornell University, Ithaca NY 14850
+%
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
+% ----------------------------------------------------------------------------------- %
+%
+% ----------------------------------------------------------------------------------- %
+% Balances: Evaluates model equations given time, state and the data_dictionary.
+% Type: GRN-JULIA
+% Version: 1.0
+%
+% Input arguments:
+% t  - current time
+% x  - state array
+% data_dictionary  - Data dictionary instance (holds model parameters)
+%
+% Return arguments:
+% dxdt - derivative array at current time step
+% ----------------------------------------------------------------------------------- %
+function dxdt = Balances(x,t,data_dictionary)
+
+  % Get the stoichiometric_matrix -
+  stoichiometric_matrix = data_dictionary.stoichiometric_matrix;
+
+  % Call the kinetics function -
+  rate_array = Kinetics(t,x,data_dictionary);
+
+  % Call the control function -
+  control_array = Control(t,x,rate_array,data_dictionary);
+
+  % Call the dilution function -
+  dilution_array = Dilution(t,x,rate_array,data_dictionary);
+
+  % Modify the rate array -
+  rate_array = rate_array.*control_array;
+
+  # calculate the dxdt for chemical species -
+  dxdt = stoichiometric_matrix*rate_array+dilution_array;
+  
+return
---
+++ make_octave_model.jl
@@ -0,0 +1,104 @@
+using ArgParse
+using JSON
+
+include("./src/Types.jl")
+include("./src/Macros.jl")
+include("./src/Parser.jl")
+include("./src/Problem.jl")
+include("./src/strategy/OctaveStrategy.jl")
+include("./src/Common.jl")
+
+# Grab the required functions for code generation -
+# const parser_function::Function = parse_vff_file
+
+function parse_commandline()
+    settings_object = ArgParseSettings()
+    @add_arg_table settings_object begin
+      "-o"
+        help = "Directory where the Julia model files will be written."
+        arg_type = AbstractString
+        default = "."
+
+      "-m"
+        help = "Path to the biochemical reaction file written in the vff format."
+        arg_type = AbstractString
+        required = true
+
+      "-s"
+        help = "ODE solver (Default: LSODE)"
+        arg_type = Symbol
+        default = :LSODE
+
+      "-r"
+        help = "Reactor configuration (B = Batch, F = Fedbatch, C = Continous)"
+        arg_type = Symbol
+        default = :B
+    end
+
+    # return a dictionary w/args -
+    return parse_args(settings_object)
+end
+
+
+function main()
+
+  # Build the arguement dictionary -
+  parsed_args = parse_commandline()
+
+  # Load the statement_vector -
+  path_to_model_file = parsed_args["m"]
+  statement_vector::Array{VFFSentence} = parse_vff_file(path_to_model_file)
+
+  # Generate the problem object -
+  problem_object = generate_problem_object(statement_vector)
+
+  # Load the JSON configuration file -
+  config_dict = JSON.parsefile("./config/Configuration.json")
+  problem_object.configuration_dictionary = config_dict
+
+  # Write the DataDictionary -
+  solver_type = parsed_args["s"]
+  reactor_type = parsed_args["r"]
+  component_set = Set{ProgramComponent}()
+  program_component_data_dictionary = build_data_dictionary_buffer(problem_object,solver_type,reactor_type)
+  push!(component_set,program_component_data_dictionary)
+
+  # Write the dilution function if this is a Fedbatch -
+  if (reactor_type == :F)
+
+    # Write the dilution -
+    program_component_dilution = build_dilution_buffer(problem_object,solver_type,reactor_type)
+    push!(component_set,program_component_dilution)
+  end
+
+  # Write the Kinetics -
+  program_component_kinetics = build_kinetics_buffer(problem_object,solver_type)
+  push!(component_set,program_component_kinetics)
+
+  # Write the Inputs -
+  program_component_inputs = build_inputs_buffer(problem_object)
+  push!(component_set,program_component_inputs)
+
+  # Write the stoichiometric_matrix --
+  program_component_stoichiometric_matrix = generate_stoichiomteric_matrix_buffer(problem_object)
+  push!(component_set,program_component_stoichiometric_matrix)
+
+  # Dump the component_set to disk -
+  path_to_output_file = parsed_args["o"]
+  write_program_components_to_disk(path_to_output_file,component_set)
+
+  # Transfer distrubtion files to the output -
+  if (reactor_type == :B)
+
+    transfer_distribution_file("./distribution","Solve.m",path_to_output_file,"Solve.m")
+    transfer_distribution_file("./distribution","Driver.m",path_to_output_file,"Driver.m")
+
+  elseif (reactor_type == :F)
+
+    transfer_distribution_file("./distribution","SolveFedbatch.m",path_to_output_file,"Solve.m")
+    transfer_distribution_file("./distribution","DriverFedbatch.m",path_to_output_file,"Driver.m")
+
+  end
+end
+
+main()
